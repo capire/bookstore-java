474e5e459d7c5907d699276a5994beb139882ec7
Vladislav Leonkev <vladislav.leonkev@sap.com>
2026-01-07 16:43:24 +0100

    add assertions

--- srv/src/main/java/customer/bookstore/CatalogHandler.java
+++ srv/src/main/java/customer/bookstore/CatalogHandler.java
@@ -6,8 +6,8 @@ import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.Result;
 import com.sap.cds.Row;
-import com.sap.cds.ql.CQL;
 import com.sap.cds.ql.Select;
+import com.sap.cds.ql.CQL;
 import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.Update;
 import com.sap.cds.ql.cqn.CqnUpdate;
@@ -21,8 +21,8 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.On;
 
-import cds.gen.catalogservice.Books;
-import cds.gen.catalogservice.Books_;
+import cds.gen.sap.capire.bookshop.Books;
+import cds.gen.sap.capire.bookshop.Books_;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Currencies;
 import cds.gen.catalogservice.ListOfBooks;
@@ -46,7 +46,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import java.text.SimpleDateFormat;
 import com.sap.cds.services.messaging.TopicMessageEventContext;
 import com.sap.cds.services.outbox.OutboxService;
-
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
@@ -84,19 +83,17 @@ public class CatalogHandler implements EventHandler {
   }
 
   @On(event = SubmitOrderContext.CDS_NAME)
-  public SubmitOrderContext.ReturnType submitOrder(SubmitOrderContext context) {
+  public void submitOrder(SubmitOrderContext context) {
     Integer quantity = context.getQuantity();
     if(quantity<=0)
       throw new ServiceException(ErrorStatuses.BAD_REQUEST, "quantity has to be 1 or more").messageTarget("submitOrder");
-    Integer booksLeft = updateBookQuantity(context.getBook(), -context.getQuantity());
-    SubmitOrderContext.ReturnType response = SubmitOrderContext.ReturnType.create();
-    response.setStock(booksLeft);
-    return response;
+    updateBookQuantity(context.getBook(), context.getQuantity());
+    context.setCompleted();
   }
 
   @After(event = SubmitOrderContext.CDS_NAME)
   public void submitOrderAfter(SubmitOrderContext context) {
-    CqnSelect selectBook = Select.from(CatalogService_.BOOKS)
+    CqnSelect selectBook = Select.from(Books_.class)
       .columns(c -> c.title(),
               c -> c.price(),
             c -> c.currency().code())
@@ -150,24 +147,19 @@ public class CatalogHandler implements EventHandler {
     }
   }
 
-  private Integer updateBookQuantity(Integer bookId, Integer deltaQuantity) {
-    Result found = persistenceService.run(Select
-      .from(Books_.CDS_NAME)
-      .where(b -> b.get("ID").eq(bookId)));
-    Books book = found.single(Books.class);
-    Integer inStock = book.getStock();
-
-    Integer booksLeft = inStock+deltaQuantity;
-    if(booksLeft<0)
-      throw new ServiceException(ErrorStatuses.BAD_REQUEST, "Book("+bookId+") " + deltaQuantity + " exceeds stock").messageTarget("submitOrder");
-
-    CqnUpdate update = Update.entity(Books_.CDS_NAME)
-      .byId(book.getId())
-      .set(Books.STOCK, CQL.get(Books.STOCK).plus(deltaQuantity));
-
+  private void updateBookQuantity(Integer bookId, Integer quantity) {
+    CqnUpdate update = Update.entity(Books_.class)
+      .set(Books.STOCK, CQL.get(Books.STOCK).minus(quantity))
+      .where(b -> b.get(Books.ID).eq(bookId)
+      .and(b.get(Books.STOCK).ge(quantity)));
     Result updateResult = persistenceService.run(update);
-    if(updateResult.rowCount()!=1)
-      throw new ServiceException(ErrorStatuses.SERVER_ERROR, "Failed to update the book quantity");
-    return booksLeft;
+    if(updateResult.rowCount()==0) {
+      CqnSelect select = Select.from(cds.gen.sap.capire.bookshop.Books_.CDS_NAME).byId(bookId); 
+      Result selectResult = persistenceService.run(select);
+      if(selectResult.rowCount()==0)
+        throw new ServiceException(ErrorStatuses.CONFLICT, "Book not found");
+      throw new ServiceException(ErrorStatuses.CONFLICT, "Quantity exceeds stock");
+    }
   }
+
 }
